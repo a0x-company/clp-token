8065016938bec1e41224c5e9f29002253f8dbeaf
tesslaray <cristian.valdivia@alumnos.usm.cl>
2024-10-02 20:01:12 -0300

    feat: improve navbar and send to reserve page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,38 +1,45 @@
+'use client';
+
 // react
-import React from "react";
+import React from 'react';
 
 // next
-import Link from "next/link";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 // components
-import { Button } from "./ui/button";
+import { Button } from './ui/button';
 
 // translations
-import { useTranslations } from "next-intl";
+import { useTranslations } from 'next-intl';
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
-  const t = useTranslations("navbar");
+  const t = useTranslations('navbar');
+  const pathname = usePathname();
+
+  const currentLang = pathname.startsWith('/es') ? 'es' : 'en';
+
   return (
     <nav className="bg-brand-blue px-12 py-4 flex justify-between items-center sticky top-0 z-50">
       <div className="text-white font-bold text-xl">LOGO HERE</div>
       <div className="flex flex-1 space-x-4 items-center justify-evenly font-bold font-helvetica">
         <Button className="bg-white text-black h-auto px-6 py-2 text-xl rounded-[12px] border-2 border-black font-bold shadow-brutalist">
-          {t("stableCoin")}
+          {t('stableCoin')}
         </Button>
         <Link href="#" className="text-white text-xl hover:text-blue-200">
-          {t("earnWithPesos")}
+          {t('earnWithPesos')}
         </Link>
         <Link href="#" className="text-white text-xl hover:text-blue-200 ">
-          {t("aboutUs")}
+          {t('aboutUs')}
         </Link>
-        <Link href="#" className="text-white text-xl hover:text-blue-200">
-          {t("reserve")}
+        <Link href={`/${currentLang}/reserve`} className="text-white text-xl hover:text-blue-200">
+          {t('reserve')}
         </Link>
       </div>
       <Button className="bg-black text-white h-auto px-6 py-2 text-xl rounded-[12px] border-2 border-black font-bold shadow-brutalist">
-        {t("login")}
+        {t('login')}
       </Button>
     </nav>
   );
