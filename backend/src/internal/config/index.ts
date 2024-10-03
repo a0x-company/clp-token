4b85897a7fd4db0ae6ac8fd8c3fa4bfb2f96d34e
Claudio Condor <claucondor@gmail.com>
2024-10-03 12:55:04 -0400

    feat: configure server an api key middleware

--- backend/src/internal/config/index.ts
+++ backend/src/internal/config/index.ts
@@ -2,7 +2,7 @@
 export const PROJECT_ID = process.env.PROJECT_ID;
 export const DATABASE_ENV = process.env.DATABASE_ENV || "(default)";
 
-export const A0X_API_KEY = process.env.A0X_API_KEY;
+export const API_KEY = process.env.API_KEY;
 
 export const VAULT_RUT = process.env.VAULT_RUT;
 export const VAULT_PASSWORD = process.env.VAULT_PASSWORD;
@@ -15,11 +15,11 @@ function stopProgram(envKey: string) {
 // validation
 export function validateRequiredEnvs() {
   if (!PROJECT_ID) stopProgram("PROJECT_ID");
-  if (!A0X_API_KEY) stopProgram("A0X_API_KEY");
+  if (!API_KEY) stopProgram("API_KEY");
 }
 
 export function validateVaultApiEnvs() {
-  if (!A0X_API_KEY) stopProgram("A0X_API_KEY");
+  if (!API_KEY) stopProgram("API_KEY");
   if (!VAULT_RUT) stopProgram("VAULT_RUT");
   if (!VAULT_PASSWORD) stopProgram("VAULT_PASSWORD");
 }
--- backend/src/internal/index.ts
+++ backend/src/internal/index.ts
@@ -1,3 +1,5 @@
 export * as config from "./config";
 
+export * as http from "./http";
+
 export * as bankScrap from "./bank-scrap";
