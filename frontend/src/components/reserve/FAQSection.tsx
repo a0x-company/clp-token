2a0f06a1723e1e080617b6ab9e63b1cb6f935aa1
MatiasPP96 <matiaas.p@gmail.com>
2024-10-04 21:23:46 -0300

    fix: copy reserves

--- frontend/src/components/reserve/FAQSection.tsx
+++ frontend/src/components/reserve/FAQSection.tsx
@@ -9,39 +9,34 @@ import Image from "next/image";
 const items = [
   {
     title:
-      "Cómo se garantiza que CLPD está completamente respaldado y es intercambiable por pesos chilenos tradicionales?",
+      "¿Cómo se garantiza que CLPD está completamente respaldado y es intercambiable por pesos chilenos tradicionales?",
     content:
-      "CLPD es un peso chileno digital respaldado 100% por dinero líquido (no está convertido en ningún tipo de activo más que CLP tradicional) y es intercambiable 1:1 por pesos chilenos fiat CLP. Todos los pesos chilenos fiat se resguardan en una cuenta bancaria nacional que es monitoreada 24/7 por oráculos independientes proporcionados por la tecnología de Chainlink Functions.",
+      "CLPD es un peso chileno digital respaldado 100% por dinero líquido (no está convertido en ningún tipo de activo más que CLP tradicional) y es intercambiable 1:1 por pesos chilenos fiat. Todos los pesos chilenos fiat se resguardan en una cuenta bancaria nacional que es monitoreada 24/7 por oráculos independientes proporcionados por la tecnología de Chainlink Functions.",
   },
   {
-    title: "Cómo puedo obtener CLPD y convertirlo a pesos chilenos fiat?",
+    title: "¿Cómo puedo obtener CLPD y convertirlo a pesos chilenos fiat?",
     content:
-      "Individuos y usuarios comunes, startups y pequeños negocios pueden obtener CLPD a través del sitio web pesodigital.mistokens.com y pronto en más aplicaciones y exchanges nacionales e internacionales. Para exchanges, instituciones, bancos, wallets, traders institucionales, grandes empresas e instituciones financieras, pueden ponerse en contacto con el equipo de CLPD.",
+      "Individuos y usuarios comunes, startups y pequeños negocios pueden obtener CLPD a través del sitio web pesodigital.mistokens.com y pronto en más aplicaciones y exchanges nacionales e internacionales. Para exchanges, instituciones, bancos, wallets, traders institucionales, grandes empresas e instituciones financieras, pueden ponerse en contacto con el equipo de desarrollador A0x.",
   },
   {
-    title: "En qué blockchain está disponible CLPD?",
+    title: "¿En qué blockchain está disponible CLPD?",
     content:
-      "CLPD está disponible en la red blockchain BASE, una solución de capa 2 en Ethereum, compatible con EVM, construida sobre el superstack de Optimism, con la seguridad, estabilidad y escalabilidad necesaria para obtener transacciones de bajísimo costo para todas las aplicaciones de pago y financieras de CLPD. Pronto se habilitará el CLPD en otras blockchains para facilitar aún más su uso y adopción.",
+      "CLPD está disponible en la red blockchain BASE, una solución de capa 2 en Ethereum, compatible con EVM (Ethereum Virtual Machine), construida sobre el superstack de Optimism, con la seguridad, estabilidad y escalabilidad necesaria para obtener transacciones de bajísimo costo para todas las aplicaciones de pago y financieras de CLPD. Pronto se habilitará el CLPD en otras blockchains para facilitar aún más su uso y adopción.",
   },
   {
-    title: "Cómo se mantiene la paridad entre el CLPD circulante y las reservas de CLP fiat?",
+    title: "¿Cómo se mantiene la paridad entre el CLPD circulante y las reservas de CLP fiat?",
     content:
-      "Cuando un usuario deposita pesos chilenos desde su cuenta bancaria a la de CLPD se crean los CLPD equivalentes en su billetera digital de manera automatizada. No existe otro mecanismo de creación de CLPD. Al mismo tiempo, cuando se quieren intercambiar CLPD por pesos chilenos fiat en la cuenta bancaria del usuario, dichos CLPD son “quemados” y sacados de circulación. Mediante este mecanismo se mantiene una paridad 1:1 en todo momento y de manera automática.",
+      "Cuando un usuario deposita pesos chilenos desde su cuenta bancaria a la de la moneda estable, se crean los CLPD equivalentes en su billetera digital de manera automatizada. No existe otro mecanismo de creación de CLPD. Al mismo tiempo, cuando se quieren intercambiar CLPD por pesos chilenos fiat en la cuenta bancaria del usuario, dichos CLPD son “quemados” y sacados de circulación. Mediante este mecanismo se mantiene una paridad 1:1 en todo momento y de manera automática.",
   },
   {
-    title: "El CLPD es una criptomoneda?",
+    title: "¿El CLPD es una criptomoneda?",
     content:
-      "El CLPD is una moneda estable 100% reservada, que es un tipo de criptomoneda, o peso chileno digital. A diferencia de otras criptomonedas que fluctúan en su precio, el CLPD está diseñado para mantener la relación de mercado con el dólar norteamericano, y tener un equivalente en pesos chilenos tradicionales respaldado 1:1 en una cuenta bancaria nacional 100% liquida. El CLPD es un equivalente al peso chileno tradicional que se beneficia de la velocidad y seguridad de la tecnología blockchain.",
+      "El CLPD es una moneda estable 100% respaldada, que es un tipo de criptomoneda, o peso chileno digital. A diferencia de otras criptomonedas que fluctúan en su precio, el CLPD está diseñado para mantener la relación de mercado con el dólar norteamericano USD, y tener un equivalente en pesos chilenos tradicionales respaldado 1:1 en una cuenta bancaria nacional 100% líquida. El CLPD es un equivalente al peso chileno tradicional que se beneficia de la velocidad y seguridad de la tecnología blockchain.",
   },
   {
-    title: "Cómo es diferente el CLPD a una CBDC (moneda digital emitida por el banco central)?",
+    title: "¿Cómo es diferente el CLPD a una CBDC (moneda digital emitida por el banco central)?",
     content:
-      "La tokenización de activos es legal en Chile, siempre y cuando se cumplan con las normativas y regulaciones aplicables. Para saber mas puedes visitar el sitio CMF y la nueva ley Fintech",
-  },
-  {
-    title: "¿Cómo puedo tokenizar mis propios activos en mistokens.com?",
-    content:
-      "El CLPD is emitido por A0x, una empresa privada chilena en la intersección de blockchain y fintech. Mientras que una CBDC sería emitida por el gobierno o el banco central. Mientras la CBDC chilena se encuentra en etapa de estudio y exploración, sin una determinación al respecto de si se creará en algún momento o no, el CLPD ya se encuentra disponible y será útil para pagos entre personas, comercios, transferencias internacionales, acceder a rendimientos de DeFi, vehículos de ahorro e inversión, y otras aplicaciones de manera segura, rápida y de bajísimo costo. A0x ha desarrollado esta tecnología basandose en las prácticas de código abierto y tomando el conocimiento del uso de las stablecoins en el mundo.",
+      "El CLPD is emitido por A0x, una empresa privada chilena en la intersección de blockchain y fintech. En cambio, una CBDC sería emitida por el gobierno o el banco central. Mientras la CBDC chilena se encuentra en etapa de estudio y exploración, sin una determinación al respecto de si se creará en algún momento o no, el CLPD ya se encuentra disponible y será útil para pagos entre personas, comercios, transferencias internacionales, acceder a rendimientos en DeFi, vehículos de ahorro e inversión, y otras aplicaciones de manera segura, rápida y de bajísimo costo. A0x ha desarrollado esta tecnología basandose en las prácticas de código abierto y tomando el conocimiento del uso de las stablecoins en el mundo.",
   },
 ];
 
